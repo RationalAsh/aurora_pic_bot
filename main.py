6069782c76a9a6afb5ff967c49db58027a3a6968
RationalAsh <ashwinnarayan1994@gmail.com>
2016-05-27 18:57:34 +0530

    Updated the welcome message

--- main.py
+++ main.py
@@ -8,8 +8,26 @@ import logging as lg
 
 TOKEN = '236423438:AAH_98YQwozj62kufw1z2sON3vtHjR_TYK0'
 
+#States of the bot
+START = 0
+PIC
+
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Hello sexy, I love Aurora,  and hence you! I'll send you beautiful pictures of yourself whenever you need me to ;)")
+    """Start off things with a welcome message and a description
+    of what the bot does."""
+    pratibha_id = ''
+    sender_name = telegram.message.user.first_name
+    welcome_text = 'Hello Aurora! I\'m a bot that find and sends'+\
+                   'you beutiful pictures of aurorae! Ashwin made'+\
+                   'me as a gift to you because he knows how much '+\
+                   'you love aurorae! Have fun!' + \
+                   telegram.emoji.Emoji.SMILING_FACE_WITH_SMILING_EYES +\
+                   '\n\n What do you want to do?'
+    custom_keyboard = [[ telegram.KeyboardButton('Give me pictures :D!'),
+                         telegram.KeyboardButton('Nothing Right Now')]]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, resize_keybaord=True,
+                                                one_time_keyboard=True)
+    bot.sendMessage(chat_id=update.message.chat_id, text=welcome_text, reply_markup=reply_markup)
 
 if __name__=='__main__':
     lg.basicConfig(level=lg.DEBUG,
