8ef9d18e0916aebbd92332c10f16407c56c81737
RationalAsh <ashwinnarayan1994@gmail.com>
2016-05-27 13:59:37 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import telegram
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+import logging as lg
+
+TOKEN = '236423438:AAH_98YQwozj62kufw1z2sON3vtHjR_TYK0'
+
+def start(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text='Hello! I love Aurorae!')
+
+if __name__=='__main__':
+    lg.basicConfig(level=lg.DEBUG,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    bot = telegram.Bot(token=TOKEN)
+    print(bot.getMe())
+    
+    updater = Updater(token=TOKEN)
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+    
+    updater.start_polling()
